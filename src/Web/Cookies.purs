3b6caf8488defdc095a6e54ad0ea85365f5151f6
dbushenko <d.bushenko@gmail.com>
2016-10-08 10:21:46 +0300

    Updated for latest purescript release -- 0.10.1

--- src/Web/Cookies.purs
+++ src/Web/Cookies.purs
@@ -6,8 +6,8 @@ module Web.Cookies (
          ) where
 
 import Prelude
-import Control.Monad.Eff
-import Data.Maybe
+import Control.Monad.Eff (Eff)
+import Data.Maybe (Maybe(..))
 
 foreign import data COOKIE :: !
 
@@ -21,8 +21,8 @@ getCookie :: forall eff value. String -> Eff (cookie :: COOKIE | eff) (Maybe val
 getCookie key = do
     cook <- _getCookie key
     prepare cook
-    where prepare [] = return Nothing
-          prepare [value] = return $ Just value
+    where prepare [value] = pure $ Just value
+          prepare _ = pure Nothing
 
 -- |  Delete cookie with specified name
 foreign import deleteCookie :: forall eff. String -> Eff (cookie :: COOKIE | eff) Unit
